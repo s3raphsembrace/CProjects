7843ab1541a219b367d62dc997e07bf39f91b351
Matthew <kmatthew8363@gmail.com>
2025-02-22 14:42:08 -0500

    Delete kuan_matthew directory

--- kuan_matthew/Day 1/prog.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("Hello World! \n Today is a good day. \n");
-	return 0;
-}
--- kuan_matthew/Day 2/day2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	int x = 9, y = 3;
-	printf("%d / %d = %f",x,y,(float)x/y)
-	return 0;
-}
